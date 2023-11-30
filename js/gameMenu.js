7caf86716b0cc62080c3a3225a36ee889f2be50f
KyLLlHEP <kushnerartur@gmail.com>
2023-11-30 12:16:54 +0000

    feat: start logic for game

--- js/gameMenu.js
+++ js/gameMenu.js
@@ -1,3 +1,5 @@
+import { startGame } from "./startGame.js";
+
 export const createGameMenu = () => {
   const title = document.createElement("h2");
   const gameSection = document.querySelector(".game-section__container");
@@ -12,7 +14,7 @@ export const createGameMenu = () => {
     button.classList.add("game-menu__difficult-btn");
     button.textContent = `${difficult} card`;
 
-    button.addEventListener("click", () => {});
+    button.addEventListener("click", () => startGame(difficult));
 
     return button;
   };
---
+++ js/startGame.js
@@ -0,0 +1,5 @@
+export const startGame = (difficult) => {
+  let firstCard = null;
+  let secondCard = null;
+  let clickable = true;
+};
